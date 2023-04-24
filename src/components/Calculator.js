1fb329ee9dc516c5056c1208b1c2bf8682d2f97c
prius2055 <prius2055@gmail.com>
2023-04-24 21:05:37 +0100

    Create a calculator component. Add stylings to the calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import './Calculator.css';
+
+const Calculator = () => (
+  <div className='calculator'>
+    <table>
+      <tr className='first-row'>
+        <td colSpan='4'>0</td>
+      </tr>
+      <tr className='other-rows'>
+        <td>AC</td>
+        <td>+/-</td>
+        <td>%</td>
+        <td className='last-data'>/</td>
+      </tr>
+      <tr className='other-rows'>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+        <td className='last-data'>x</td>
+      </tr>
+      <tr className='other-rows'>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td className='last-data'>-</td>
+      </tr>
+      <tr className='other-rows'>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td className='last-data'>+</td>
+      </tr>
+      <tr className='other-rows'>
+        <td colSpan='2'>0</td>
+        <td>.</td>
+        <td className='last-data'>=</td>
+      </tr>
+    </table>
+  </div>
+);
+
+export default Calculator;
+
+
+
+
+
