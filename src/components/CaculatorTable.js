99ec1efec66ff01f9436db658f5bb01fbe909ee7
prius2055 <prius2055@gmail.com>
2023-04-25 09:44:56 +0100

    Create and style Caculator component. Create and style CalculatorTable component as a child component of Calculator

---
+++ src/components/CaculatorTable.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './CalculatorTable.css';
+
+const CalculatorTable = () => (
+    <table>
+      <tr className='first-row'>
+        <td colSpan='4'>0</td>
+      </tr>
+      <tr className='other-rows'>
+        <td>AC</td>
+        <td>+/-</td>
+        <td>%</td>
+        <td className='last-data'>/</td>
+      </tr>
+      <tr className='other-rows'>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+        <td className='last-data'>x</td>
+      </tr>
+      <tr className='other-rows'>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td className='last-data'>-</td>
+      </tr>
+      <tr className='other-rows'>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td className='last-data'>+</td>
+      </tr>
+      <tr className='other-rows'>
+        <td colSpan='2'>0</td>
+        <td>.</td>
+        <td className='last-data'>=</td>
+      </tr>
+    </table>
+  );
+
+export default CalculatorTable;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,49 +1,11 @@
 import React from 'react';
-
+import CalculatorTable from './CaculatorTable';
 import './Calculator.css';
 
-const Calculator = () => (
-  <div className='calculator'>
-    <table>
-      <tr className='first-row'>
-        <td colSpan='4'>0</td>
-      </tr>
-      <tr className='other-rows'>
-        <td>AC</td>
-        <td>+/-</td>
-        <td>%</td>
-        <td className='last-data'>/</td>
-      </tr>
-      <tr className='other-rows'>
-        <td>7</td>
-        <td>8</td>
-        <td>9</td>
-        <td className='last-data'>x</td>
-      </tr>
-      <tr className='other-rows'>
-        <td>4</td>
-        <td>5</td>
-        <td>6</td>
-        <td className='last-data'>-</td>
-      </tr>
-      <tr className='other-rows'>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-        <td className='last-data'>+</td>
-      </tr>
-      <tr className='other-rows'>
-        <td colSpan='2'>0</td>
-        <td>.</td>
-        <td className='last-data'>=</td>
-      </tr>
-    </table>
-  </div>
-);
-
+const Calculator = () =>(
+    <div className='calculator'>
+      <CalculatorTable />
+    </div>
+  )
 export default Calculator;
-
-
-
-
 
